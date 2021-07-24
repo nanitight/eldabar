6bd17c0275ffe8f459c3e22919d69ed64c841944
nanitight <ntpnaane@gmail.com>
2021-07-24 15:12:05 +0200

    added global css and changed link color to white

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from  'react' ;
+import '../css/global.css'
 function About(){
     function openClose(){
         var btn = document.getElementById('more');
@@ -37,24 +38,24 @@ function About(){
             <li id="4">Sound Family Unit</li>
         </ul>
     </p>
-    <p>
+    <p className="read-text">
     El Dabar City Church is a Bible base Messianic Community 
     that seeks to empower people by communicating the Good News 
     of the Kingdom of Yeshua the Messiah in a scriptural illustrative 
     and simple way. We are a Royal Priesthood. Messiah is the Centre... 
     </p>
     <span id="more" className="click-btn" onClick={openClose}>More...</span>
-    <text id="more-txt" style={{display:'none'}}> EL-DABAR CITY CHURCH (Latter House Community International ( LHCI ), we believe on the partnership of  Priests (those who are called in the five fold ministries or serving in the church full time)   and Kings ( those whom God has called to serve in the market place / secular). We believe both kings and priests are called and anointed by God for the advancement of the Kingdom of God.
-
+    <text className="read-text" id="more-txt" style={{display:'none'}}> EL-DABAR CITY CHURCH (Latter House Community International ( LHCI ), we believe on the partnership of  Priests (those who are called in the five fold ministries or serving in the church full time)   and Kings ( those whom God has called to serve in the market place / secular). We believe both kings and priests are called and anointed by God for the advancement of the Kingdom of God.
+    <br/>
 We therefore endorse that Believers who work in the market place as professionals or business people are equally called and anointed by God in the market place or secular world as the priests are called and anointed by God to serve in the church.
+<br/>
+We believe that those who works at  are serving God full time in the market place as Priestley kings. We are kingdom of Priests... <br/>
 
-We believe that those who works at  are serving God full time in the market place as Priestley kings. We are kingdom of Priests... 
+We therefore equip kings for  market place and priests for the senctuary.<br/>
 
-We therefore equip kings for  market place and priests for the senctuary.
-
-We believe that Priests are called to give a vision in the Kingdom and kings are called to provide for the Kingdom vision.  
+We believe that Priests are called to give a vision in the Kingdom and kings are called to provide for the Kingdom vision.  <br/>
 So the work of the priest is to bless the king to succeed in the market place battle, and the work of the king is to take the spoil in the market place and bring them in the Kingdom of God.
-In the Kingdom of God all are full time, whether as a pastor or medical doctor.   
+In the Kingdom of God all are full time, whether as a pastor or medical doctor.   <br/>
 WE ARE A FAMILY OF KINGS AND PRIESTS PARTNERING FOR THE ADVANCEMENT OF THE KINGDOM OF GOD.  </text>
 </div>)
 }
